efc4bafabf80f7203405cb69ec09d81720c5f7b7
Noam Atzmon <noam.atzmon@post.idc.ac.il>
2023-12-19 21:16:32 +0200

    GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -4,17 +4,19 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		int Min = 50;
-		int Max = 100;
+		int Min = Integer.parseInt(args[0]);
+		int Max = Integer.parseInt(args[1]);
 		int fin;
 		int x = Min + (int)(Math.random() * ((Max - Min) + 1));
 		int y = Min + (int)(Math.random() * ((Max - Min) + 1));
 		int z = Min + (int)(Math.random() * ((Max - Min) + 1));
-		System.out.println("Out of the three integers:" + x + ", " + y + ", " + z);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
 		int first = Math.min(x, y);
 		int second = Math.min(y, z);
 		fin = Math.min(first, second);
-		System.out.println(fin + " is the lowest number");
+		System.out.println("The minimal generated number was " + fin);
 
 	}
 }
