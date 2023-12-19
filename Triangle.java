00dec1e6899c1ff28a8caaa53d13a151697afa69
Noam Atzmon <noam.atzmon@post.idc.ac.il>
2023-12-19 19:06:57 +0200

    Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,17 @@
+/*
+ * Three sides can form a triangle if the sum of the lengths of any two sides is greater than the length of the remaining side. 
+ * This is known as the Triangle Inequality Theorem. 
+ * Write a program that tests if three given integers form a triangle.
+ */ 
+public class Triangle {
+	public static void main(String[] args) {
+		int s1 = Integer.parseInt(args[0]);
+		int s2 = Integer.parseInt(args[1]);
+		int s3 = Integer.parseInt(args[2]);
+		if (((s1 + s2) > s3) && ((s2 + s3) > s1) && ((s1 + s3) > s2)) {
+		System.out.println("The given sides: " + s1 + ", " + s2 + ", " + s3 + " do form a triangle");
+		} else {
+		System.out.println("The given sides: " + s1 + ", " + s2 + ", " + s3 + " do not form a triangle");
+		}
+	}
+}
