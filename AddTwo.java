b0c3f9bb918f96e95e2ea502bd975c322a23b2c0
Noam Atzmon <noam.atzmon@post.idc.ac.il>
2023-12-19 21:05:28 +0200

    AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -5,12 +5,6 @@ public class AddTwo {
 	public static void main(String[] args) {
 		int x = Integer.parseInt(args[0]);
 		int y = Integer.parseInt(args[1]);
-		System.out.println("The addition of two variables is ");
-		System.out.println("               " + x + "                 ");
-		System.out.println("                +                 ");
-		System.out.println("                " + y + "                 ");
-		System.out.println("                =                 ");
-		System.out.println("               " + (x + y) + "                 ");
-
+		System.out.println(x + " + " + y + " = " + (x+y));
 	}
 }
