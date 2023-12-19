70877ab7d5d8078d46925f02bfaa99f113f00709
Noam Atzmon <noam.atzmon@post.idc.ac.il>
2023-12-19 18:38:13 +0200

    LinearEq.java

---
+++ LinearEq.java
@@ -0,0 +1,17 @@
+/*
+ * Solves linear equations of the form a⋅x + b = c.
+ * The program gets a, b, and c as command-line arguments,
+ * computes x, and prints the result.
+ * Treats the three arguments as well as the computed value as double values
+ */
+public class LinearEq {
+	public static void main(String[] args) {
+	double a = Double.parseDouble(args[0]);
+	double b = Double.parseDouble(args[1]);
+	double c = Double.parseDouble(args[2]);
+	double fin;
+	fin = (c - b)/a;
+	System.out.println("The solution to the equation " + a + "x + " + b + " = " + c + " is:");
+	System.out.println(fin);
+	}
+}
