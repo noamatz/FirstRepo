1db87efffeee07fb5b661433c18b38d7da002d23
Noam Atzmon <noam.atzmon@post.idc.ac.il>
2023-12-19 17:56:25 +0200

    AddTwo.java

---
+++ AddTwo.java
@@ -0,0 +1,18 @@
+/*
+ * Adds two given integers and prints the result in a fancy way.
+ */
+public class AddTwo {
+	public static void main(String[] args) {
+		int x;
+		int y;
+		x = 10;
+		y = 5;
+		System.out.println("The addition of two variables is ");
+		System.out.println("               10                 ");
+		System.out.println("                +                 ");
+		System.out.println("                5                 ");
+		System.out.println("                =                 ");
+		System.out.println("               " + (x + y) + "                 ");
+
+	}
+}
